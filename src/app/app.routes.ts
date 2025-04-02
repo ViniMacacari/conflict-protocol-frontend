6b32c264de6093f3ff4f8d4e02afa818b346503d
ViniMacacari <macacarivinicius@gmail.com>
2025-04-02 00:06:23 -0300

    feat: criação de componentes-página e componentes padrão (botão e input)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
+import { RulesComponent } from './pages/rules/rules.component'
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'regras', component: RulesComponent }
 ]
