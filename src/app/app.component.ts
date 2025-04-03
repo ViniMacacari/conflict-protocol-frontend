870dab41e496760c174744b97958a489188b4c7b
ViniMacacari <macacarivinicius@gmail.com.br>
2025-04-03 13:25:37 -0300

    feat: animação zoom-in/zoom-out ao trocar de tela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,14 +19,14 @@ import {
   animations: [
     trigger('routeAnimations', [
       transition('* <=> *', [
-        query(':enter, :leave', style({ position: 'absolute', width: '100%' }), { optional: true }),
+        query(':enter, :leave', style({ position: 'absolute', width: '100%', height: '100%' }), { optional: true }),
         group([
           query(':leave', [
-            animate('400ms ease', style({ opacity: 0, transform: 'translateX(-20px)' }))
+            animate('400ms ease', style({ opacity: 0, transform: 'scale(0.7)' }))
           ], { optional: true }),
           query(':enter', [
-            style({ opacity: 0, transform: 'translateX(20px)' }),
-            animate('400ms ease', style({ opacity: 1, transform: 'translateX(0)' }))
+            style({ opacity: 0, transform: 'scale(1.3)' }),
+            animate('400ms ease', style({ opacity: 1, transform: 'scale(1)' }))
           ], { optional: true })
         ])
       ])
