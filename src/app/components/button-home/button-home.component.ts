fefccf3ea2210d11078816611ec79b16b64970a7
ViniMacacari <macacarivinicius@gmail.com>
2025-04-03 01:27:59 -0300

    feat: criação de começo da estilização da página home

--- src/app/components/button-home/button-home.component.ts
+++ src/app/components/button-home/button-home.component.ts
@@ -9,4 +9,5 @@ import { Component, Input } from '@angular/core'
 })
 export class ButtonHomeComponent {
   @Input() text: string = ''
+  @Input() secondText: string = ''
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonHomeComponent } from "../button-home/button-home.component"
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [ButtonHomeComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
