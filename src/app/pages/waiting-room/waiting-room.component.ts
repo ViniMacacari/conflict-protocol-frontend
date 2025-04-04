ccea8e769804b46ef5414a4fd63b113cc29ce825
ViniMacacari <macacarivinicius@gmail.com>
2025-04-04 00:50:00 -0300

    feat: componentes de sala adicionados

---
+++ src/app/pages/waiting-room/waiting-room.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-waiting-room',
+  standalone: true,
+  imports: [],
+  templateUrl: './waiting-room.component.html',
+  styleUrl: './waiting-room.component.scss'
+})
+export class WaitingRoomComponent {
+
+}
