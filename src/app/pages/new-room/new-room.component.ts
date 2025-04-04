fc75833abe24e7307e156473b7cc89fcc32d2a7d
ViniMacacari <macacarivinicius@gmail.com>
2025-04-04 00:36:48 -0300

    feat: botão principal para criar sala

--- src/app/pages/new-room/new-room.component.ts
+++ src/app/pages/new-room/new-room.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InputComponent } from "../../components/input/input.component"
+import { ButtonComponent } from "../../components/button/button.component"
 
 @Component({
   selector: 'app-new-room',
   standalone: true,
-  imports: [InputComponent],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './new-room.component.html',
   styleUrl: './new-room.component.scss'
 })
